3657f3066b3512511aa051218f96beb1d6bc9fe7
alexlato <86857302+alexlato@users.noreply.github.com>
2023-07-10 21:46:37 -0700

    navbar progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
-    <div className="flex justify-between items-center h-24 mx-auto px-16 py-16 sm:px-24 text-[#71171e]">
-      <h1 className="w-full text-3xl font-bold text-[#71171e]">
-        Maximus Insurance Inc.
+    <div className="flex justify-between items-center h-24 mx-auto px-16 py-16 sm:px-24  text-[#71171e]">
+      <h1 className="w-full text-2xl text-[#71171e]">
+        Maximus Insurance Agency Inc.
       </h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Applications</li>
-        <li className="p-4">Insurance</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Support</li>
-        <li className="p-4">Contact</li>
+        <li className="p-4 flex items-center">
+          Applications <RiArrowDropDownLine size={20} />
+        </li>
+        <li className="p-4 flex items-center">
+          Insurance <RiArrowDropDownLine size={20} />
+        </li>
+        <li className="p-4 flex items-center">
+          About <RiArrowDropDownLine size={20} />
+        </li>
+        <li className="p-4 flex items-center">
+          Support <RiArrowDropDownLine size={20} />
+        </li>
+        <li className="p-4 flex items-center">Contact</li>
       </ul>
-      <div className="px-16 py-16">
+      <div className="hidden md:flex px-16 py-16">
         <button className="bg-[#5a0715] w-[180px] rounded-md font-medium my-6 mx-auto py-3 text-white">
           Get a Quote
         </button>
       </div>
+      <div onClick={handleNav} className="block md:hidden">
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
+      <ul
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[80%] h-full border-r border-r-black-400 bg-white ease-in-out duration-500"
+            : "ease-in-out duration-500 fixed left-[-100%]"
+        }
+      >
+        <li className="p-4 flex justify-center border-b border-black-400">
+          Applications <RiArrowDropDownLine size={20} />
+        </li>
+        <li className="p-4 flex justify-center border-b border-black-400">
+          Insurance <RiArrowDropDownLine size={20} />
+        </li>
+        <li className="p-4 flex justify-center border-b border-black-400">
+          About <RiArrowDropDownLine size={20} />
+        </li>
+        <li className="p-4 flex justify-center border-b border-black-400">
+          Support <RiArrowDropDownLine size={20} />
+        </li>
+        <li className="p-4 flex justify-center border-b border-black-400">
+          Contact
+        </li>
+      </ul>
     </div>
   );
 };
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { AiOutlineShoppingCart, AiOutlineLogin } from "react-icons/ai";
 
 const Topbar = () => {
   return (
-    <div className="flex justify-end h-4 mx-auto px-0 py-0 sm:px-40 text-sm text-[#71171e]">
+    <div className="flex justify-end h-8 mx-auto sm:px-40 border-b text-sm text-[#71171e]">
       <ul className="hidden md:flex">
-        <button className="p-4">Online Payments</button>
-        <button className="p-4">Client Portal</button>
+        <a
+          className="flex items-center p-4 border-l border-r"
+          href="https://www.securevcheck.com/insurance.php?id=maximusinsuranceagency"
+        >
+          <AiOutlineShoppingCart size={20} />
+          Online Payments
+        </a>
+        <a
+          className="flex items-center p-4 border-r"
+          href="https://www.securevcheck.com/insurance.php?id=maximusinsuranceagency"
+        >
+          <AiOutlineLogin size={20} />
+          Client Portal
+        </a>
       </ul>
     </div>
   );
